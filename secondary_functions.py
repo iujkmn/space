d24aa41430a9b34e0f9f040e953cac8cb36dba14
iujkmn <127035726+iujkmn@users.noreply.github.com>
2023-09-03 13:38:15 +0300

    Update secondary_functions.py

--- secondary_functions.py
+++ secondary_functions.py
@@ -17,11 +17,3 @@ def get_extension(url):
     url = urlsplit(str_url).path
     return (os.path.splitext(url))[1]
 
-
-def main():
-    folder_name = "images"
-    Path(folder_name).mkdir(parents=True, exist_ok=True)
-
-
-if __name__ == '__main__':
-    main()
